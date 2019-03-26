e6aee5a04fcc08717a5181b9c1ab06350aff4dbd
Carlo Jacobs <carlojacobs91@gmail.com>
2019-03-26 17:35:51 +0100

    Added .py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Loading the model
-loaded_model = keras.models.load_model("my_model.h5")
+loaded_model = keras.models.load_model("94_model.h5")
 
 graph = tf.get_default_graph()
 
@@ -29,19 +29,14 @@ def validate_from(form_data):
 	return True
 
 def prepare_form_data(form_data):
-	income = int(form_data["income"])
 	final_data = []
-	pclass = 0
+	pclass = form_data["class"]
 	ticket = 0
-	if income >= 100000:
-		pclass = 1
+	if pclass == 1:
 		ticket = 60
-	elif income >= 45000:
-		pclass = 2
+	elif pclass == 2:
 		ticket = 40
-
-	elif income < 45000:
-		pclass = 3
+	elif pclass == 3:
 		ticket = 7
 
 	final_data.append(pclass)
@@ -51,6 +46,9 @@ def prepare_form_data(form_data):
 	final_data.append(int(form_data["pars"]))
 	final_data.append(ticket)
 	final_data.append(int(form_data["embarked"]))
+
+	print(final_data)
+
 	return final_data
 
 @app.route("/")
@@ -71,7 +69,9 @@ def predict():
 		return "<h1>Oops! You didn't fill in the entire form!</h1>"
 
 	data = prepare_form_data(form_data)
-	prediction = make_prediction(data)[0][1]
+	prediction = make_prediction(data)[0][0]
+
+	print(prediction)
 
 	result = np.round(100*prediction)
 	color = ""
@@ -80,18 +80,20 @@ def predict():
 	meh = False
 	sad = False
 
+	name = form_data["name"].split(' ')[0]
+
 	if prediction > 0.6:
 		color = "#7FFF00"
-		message = "Congratz! It looks like you would sursive this horrible disaster."
+		message = f'Congratz {name}! It looks like you would survive this horrible disaster.'
 		good = True
-	elif prediction >= 0.5 and result <= 0.6:
+	elif prediction >= 0.5 and prediction <= 0.6:
 		color = "orange"
-		message = "Oh... Well, try and hold your breath!"
+		message = f'Oh... Well, try and hold your breath {name}!'
 		status = "meh"
 		meh = True
 	elif prediction < 0.5:
 		color = "red"
-		message = "This is just sad... :("
+		message = f'{name}, this is just sad... :('
 		status = "sad"
 		sad = True
 
