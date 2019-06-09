71c10c6cb02c8ee9279d7c814c66ae570caba2f7
Carlo Jacobs <carlojacobs@carlos-air.home>
2019-06-09 17:27:46 +0200

    Update

---
+++ titanic_web_app/public/config/index.js
@@ -0,0 +1,9 @@
+build: {
+  // Template for index.html
+  index: path.resolve(__dirname, '../dist/index.html'),
+
+  // Paths
+  assetsRoot: path.resolve(__dirname, '../dist'),
+  assetsSubDirectory: 'static',
+  assetsPublicPath: '/',
+}
